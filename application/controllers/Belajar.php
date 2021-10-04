e5fe10879d26563e2832a5f5bdb2270e5ba815b9
letsplay09 <53568589+letsplay09@users.noreply.github.com>
2021-10-04 09:48:23 +0700

    Create Belajar.php

---
+++ application/controllers/Belajar.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Belajar extends CI_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        echo "ini method index pada controller belajar";
+    }
+
+    public function halo()
+    {
+        echo "ini method halo pada controller belajar";
+    }
+}
